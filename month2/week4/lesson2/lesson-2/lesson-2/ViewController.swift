14318dbf1efdc0dc1e977852fe180169863bb345
sher304 <esherovshermat@gmail.com>
2022-03-25 13:26:18 +0600

    adding last lesson

---
+++ month2/week4/lesson2/lesson-2/lesson-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson-2
+//
+//  Created by Шермат Эшеров on 24/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
